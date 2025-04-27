29284a14e1aadc6ffca01c1f1cb2cbff69efebde
Harshit Bamotra <harshitbamotra.01@gmail.com>
2025-04-27 16:24:18 +0530

    Error Handler

---
+++ src/repository/auth.repo.js
@@ -0,0 +1,15 @@
+const User = require("../models/user.model");
+
+class AuthRepo{
+    async register(userData){
+        try{
+
+        }
+        catch(err){
+            console.log(err);
+            
+        }
+    }
+}
+
+module.exports = AuthRepo;
